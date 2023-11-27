1cde3d96b8551a00fb66d77ea25b6137c7120132
Samara Sales <contato.samarasales@gmail.com>
2023-11-27 10:47:11 -0300

    estrutura arraylist

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        double idade = 22.50;
-        if(idade > 22.60){
-            System.out.println("maior");
-        } else if (idade == 22.50) {
-            System.out.println("igual");
-        } else{
-            System.out.println("menor");
-        }
+        int[] idades1 = new int[10];
+        idades1.length;
+
+        ArrayList<Integer> idades = new ArrayList<Integer>();
+        idades.add(22);
+        idades.add(50);
+        idades.add(30);
+        idades.remove(index: 0);
+        idades.get(1);
+        idades.size();
+
     }
 }
