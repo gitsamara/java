dfef7c5e7ed56420052f789c8cba3ffaf0ad9c57
Samara Sales <contato.samarasales@gmail.com>
2023-11-27 10:26:32 -0300

    exercicio1

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double idade = 22.50;
+        if(idade > 22.60){
+            System.out.println("maior");
+        } else if (idade == 22.50) {
+            System.out.println("igual");
+        } else{
+            System.out.println("menor");
+        }
+    }
+}
