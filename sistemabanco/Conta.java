c835f89b66138e414b8f1a6297c58063dacf8477
Samara Sales <contato.samarasales@gmail.com>
2023-12-04 08:37:49 -0300

    att testes

--- sistemabanco/Conta.java
+++ sistemabanco/Conta.java
@@ -1,30 +1,41 @@
 
 public class Conta {
 
+	// VARIAVEIS DE INSTANCIA (recebem valor padrao)
 	// o que a conta tem
 	int numeroAgencia;
-	int numeroConta;
+	private int numeroConta;
 	String nomeTitular;
-	double saldo;
+	private double saldo;
 
+	// construtor
+	 public Conta(String nomeTitular, int numeroAgencia, int numeroConta, double saldo) {
+	        this.nomeTitular = nomeTitular;
+	        this.numeroAgencia = numeroAgencia;
+	        this.numeroConta = numeroConta;
+	        this.saldo = saldo;
+	}
+
+	// VARIAVEIS LOCAIS (NAO recebem valor padrao)
 	// metodos = o que a conta faz
 	// void quer dizer que não retorna nada
 	public void depositar(double valor) {
 		if (valor >= 0) {
-			saldo = saldo + valor;
+			// saldo = saldo + valor;
+			getSaldo += valor;
 		}
 	}
 
 	public double obterSaldo() {
-		return saldo;
+		return getSaldo;
 	}
 
 	public void sacar(double valor) {
 
 		if (valor <= 0) {
 			System.out.println("Valor inválido para saque!");
-		} else if (valor > 0 && valor <= saldo) {
-			saldo = saldo - valor;
+		} else if (valor > 0 && valor <= getSaldo) {
+			getSaldo = getSaldo - valor;
 		} else {
 			System.out.println("Saldo insuficiente!");
 		}
@@ -34,6 +45,24 @@ public class Conta {
 		 * System.out.println("Saldo insuficiente!"); }
 		 */
 
+	}
+
+	// ENCAPSULAMENTO
+	// caso eu queira repetir o mesmo nome das variaveis locais e de instancia,
+	// usa-se "this.nomedavariavel" na de instancia para diferencia-la da local
+	public double getSaldo() {
+		return getSaldo;
+	}
+
+	public void setNumeroConta(int novoNumeroConta) {
+
+		if (novoNumeroConta > 0)
+			numeroConta = novoNumeroConta;
+
+	}
+
+	public int getNumeroConta() {
+		return numeroConta;
 	}
 
 }
--- sistemabanco/SistemaBanco.java
+++ sistemabanco/SistemaBanco.java
@@ -4,26 +4,39 @@ public class SistemaBanco {
 
 	public static void main(String[] args) {
 
-		Conta contaSamara = new Conta();
-		Conta contaVagner = new Conta();
-
-		System.out.println(contaSamara.saldo);
-
-		contaSamara.saldo = 8000;
+		Conta contaSamara = new Conta("Samara", 123, 456789, 0);
+		Conta contaVagner = new Conta("Vagner", 456, 987654, 0);
+		
+		/*
+		
+		//metodo get serve para pegar os valores das variaveis que estao privadas
+		System.out.println(contaSamara.getSaldo());
+		
+		double meuSaldo = contaSamara.getSaldo();
+		System.out.println(meuSaldo);
 
 		String nome = JOptionPane.showInputDialog("Digite o nome do titular da conta Samara: ");
 		contaSamara.nomeTitular = nome;
 
 		contaSamara.numeroAgencia = 123;
-		contaSamara.numeroConta = 8859700;
+		contaSamara.getNumeroConta();
 
 		double valorDeposito = 500;
 		contaSamara.depositar(valorDeposito);
 
-		System.out.println(contaSamara.saldo);
-		System.out.println(contaVagner.saldo);
+		System.out.println(contaSamara.getSaldo());
+		System.out.println(contaVagner.getSaldo());
 
 		double valorSaldo = contaSamara.obterSaldo();
+		
+		contaSamara.depositar(1000);
+		System.out.println(contaSamara.getSaldo());
+		
+		contaSamara.sacar(325);
+		System.out.println(contaSamara.getSaldo());
+		
+		contaSamara.setNumeroConta(100);
+		System.out.println(contaSamara.getNumeroConta()); */
 
 	}
 
