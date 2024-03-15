3910bb67ba4cbec5789936eb06a719fb40776d01
Samara Sales <contato.samarasales@gmail.com>
2024-03-15 10:44:12 -0300

    Delete exercicios/agenda-maven directory

--- exercicios/agenda-maven/src/main/java/com/samarasales/console1/repositorios/ContatoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.samarasales.console1.repositorios;
-
-import com.samarasales.console1.entidades.Contato;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContatoRepository extends JpaRepository<Contato, Long> {
-}
